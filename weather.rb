591c68ef60bf72f90431a869e40c34390399d66c
homanj <homanj@mkthoman.firepond.com>
2009-10-27 13:53:13 -0500

    failed attempt at using mustache to display weather conditions

--- weather.rb
+++ weather.rb
@@ -4,47 +4,38 @@ require 'yahoo-weather'
 require 'mustache/sinatra'
 
 module Weather
-	class App < Sinatra::Default
-	  register Mustache::Sinatra
-	  
-	  set :views,     'templates/'
-	  set :mustaches, 'views/' 
-	
-    helpers do
-	    def get_weather_info(zipcode)
-		    client = YahooWeather::Client.new
-		    response = client.lookup_location(zipcode)
-		    title = response.title
-		    condition = response.condition
-		    today = response.forecasts[0]
-		    day_one = response.forecasts[1]
-		    date_format = "%A %b %d"
+	class App < Sinatra::Base
+		register Mustache::Sinatra
 		
-		    "#{title}<br />
-		    Now: #{condition.temp} degrees; #{condition.text}<br /><br />
-		    Forecast:<br />
-		    #{today.date.strftime(date_format)}  #{today.text}<br />
-		    High: #{today.high} degrees<br /> 
-		    Low: #{today.low} degrees
-		    <br /><br />
+		set :views,     'templates/'
+		set :mustaches, 'views/'
 		
-		    #{day_one.date.strftime(date_format)}  #{day_one.text}<br />
-		    High: #{day_one.high} degrees<br /> 
-		    Low: #{day_one.low} degrees"
-	    end
-    end
+		enable :sessions
 
-    get '/' do 
-	    mustache :index
-    end
+		helpers do
+			def get_weather_info(zipcode)
+				client = YahooWeather::Client.new
+				response = client.lookup_location(zipcode)
+				title = response.title
+				condition = response.condition
+		
+				{ 'title' => title, 'temp' => condition.temp, 'text' => condition.text }
+			end
+		end
 
-    post '/' do
-      zipcode = params['zipcode']
-      redirect '/' + zipcode.to_s
-    end
+		get '/' do 
+			mustache :index
+		end
 
-    get %r{/(\d{5})} do |zipcode|
-	    get_weather_info(zipcode)
-    end
-  end
+		post '/' do
+			zipcode = params['zipcode']
+			redirect '/' + zipcode.to_s
+		end
+
+		get %r{/(\d{5})} do |zipcode|
+			weather = get_weather_info(zipcode)
+			
+			weather['title'] + '<p>' + weather['temp'].to_s + ' degrees; ' + weather['text'];
+		end
+	end
 end
